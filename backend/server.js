eaa8efd6fb5bb9e6680253fa8c0b713e2a9f97a7
harsha-1807 <harshavardhan.sr@kalvium.community>
2025-04-09 22:02:10 +0530

    cloudinary implemented with multer

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: ".env.local" });
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -8,11 +8,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/files", fileRoutes);
+
+
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("DB Connection Error:", err));
 
 app.use("/api/files", fileRoutes);
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
